7a78d61a440c935fd92e426d9edb2811d3d87905
Jeet009 <jeetmukherjee009@gmail.com>
2021-05-27 21:16:45 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || [
+      { todo: "Hi Jeet, hope you're good ..." },
+    ]
+  );
+  const [todo, setTodo] = useState();
+  const captureTodo = (e) => {
+    setTodo(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    todos.push({ todo });
+    localStorage.setItem("todos", JSON.stringify(todos));
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+    setTodo("");
+  };
+  const handleDelete = (name) => {
+    localStorage.setItem(
+      "todos",
+      JSON.stringify(todos.filter((todo) => todo.todo !== name))
+    );
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="form-view">
+        <div className="form-container">
+          <form>
+            <input
+              type="text"
+              placeholder="Enter Todo ..."
+              value={todo}
+              onChange={captureTodo}
+            />
+            <button className="btn btn-custom " onClick={handleSubmit}>
+              <span className="fa fa-check"></span>
+            </button>
+          </form>
+        </div>
+        <div className="todo-container">
+          {todos.map((todo) => (
+            <div className="todo">
+              <h5 onClick={() => handleDelete(todo.todo)}>{todo.todo}</h5>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
 
